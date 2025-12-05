cafc2274151fc590bc054a4cbc9f41c7345d26a3
Bishow Shrestha <bishowshrestha@Bishows-MacBook-Air.local>
2025-12-05 14:20:12 +0545

    render config file

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,7 @@ Route::middleware('auth:sanctum')->group(function () {
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login', [AuthController::class, 'login']);
 });
+
+Route::get('/documentation', function () {
+    return view('swagger');
+})->middleware('auth');
